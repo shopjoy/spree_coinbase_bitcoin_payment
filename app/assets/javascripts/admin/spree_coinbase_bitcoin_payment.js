f7691b79737878575f36d9a98c2230c4ac41f030
shopjoy <admin@shopjoy.com.au>
2014-07-09 20:09:38 +1000

    Fix admin screen

--- app/assets/javascripts/admin/spree_coinbase_bitcoin_payment.js
+++ app/assets/javascripts/admin/spree_coinbase_bitcoin_payment.js
@@ -1,1 +1,17 @@
 //= require admin/spree_backend
+
+SpreeCoinbase = {
+  hideSettings: function(paymentMethod) {
+    if (SpreeCoinbase.paymentMethodID && paymentMethod.val() == SpreeCoinbase.paymentMethodID) {
+      alert('You cannot make a Coinbase payment through the admin backend at this time.');
+    }
+  }
+}
+
+$(document).ready(function() {
+  checkedPaymentMethod = $('[data-hook="payment_method_field"] input[type="radio"]:checked');
+  SpreeCoinbase.hideSettings(checkedPaymentMethod);
+  paymentMethods = $('[data-hook="payment_method_field"] input[type="radio"]').click(function (e) {
+    SpreeCoinbase.hideSettings($(e.target));
+  });
+})
